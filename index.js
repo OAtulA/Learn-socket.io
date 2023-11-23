4d097df4180458300b8e671a7dd3f5da147eef0e
OAtulA <oraonatula@gmail.com>
2023-11-23 11:02:36 +0530

    Alright page is starting to take shape.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const {createServer} = require("http")
+const path = require("path")
 
 const app = express()
 const server = createServer(app)
 
 app.get('/', (req, res)=>{
-    res.send('<h1>Hi Devs</h1>')
+    // res.send('<h1>Hi Devs</h1>')
+    res.sendFile(path.resolve('./index.html'))
 });
 
 server.listen(4001, ()=>{
