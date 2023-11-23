b82f05cbf0f93c4bd2596b571cef457625ae6a04
OAtulA <oraonatula@gmail.com>
2023-11-23 11:14:56 +0530

    Finally my server is listening.

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require("express")
 const {createServer} = require("http")
 const path = require("path")
+const {Server} = require("socket.io")
 
 const app = express()
-const server = createServer(app)
+const server = createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res)=>{
     // res.send('<h1>Hi Devs</h1>')
     res.sendFile(path.resolve('./index.html'))
 });
+
+io.on('connection', (socket)=>{
+    console.log("A user is connected")
+})
 
 server.listen(4001, ()=>{
     console.log("Server is listening on http://localhost:4001")
